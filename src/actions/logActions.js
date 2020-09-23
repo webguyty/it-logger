22f50104a561a032d1981bd077392cf45cdb7776
Tyler Austin <tyler@webguyty.com>
2020-09-23 13:53:48 -0700

    updated logs working

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -4,6 +4,9 @@ import {
   LOGS_ERROR,
   ADD_LOG,
   DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
 } from './types';
 
 // Get logs from server
@@ -48,6 +51,7 @@ export const addLog = (log) => async (dispatch) => {
 export const deleteLog = (id) => async (dispatch) => {
   try {
     setLoading();
+
     await fetch(`./logs/${id}`, {
       method: 'DELETE',
     });
@@ -59,6 +63,44 @@ export const deleteLog = (id) => async (dispatch) => {
   } catch (err) {
     dispatch({ type: LOGS_ERROR, payload: err.response.data });
   }
+};
+
+// Update log from server
+export const updateLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`./logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+};
+
+// Set Current Log
+export const setCurrent = (log) => {
+  return {
+    type: SET_CURRENT,
+    payload: log,
+  };
+};
+
+// Clear current log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT,
+  };
 };
 
 // Set loading to true
