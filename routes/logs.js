72f7bfb82e4ddd7ec79a3169fe2bea26d95daf77
Tyler Austin <tyler@webguyty.com>
2020-09-28 19:34:14 -0700

    log post and get working

--- routes/logs.js
+++ routes/logs.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-const Logs = require('../models/logs');
+const Logs = require('../models/Logs');
 
-// @route		POST api/dailyGoals
-// @desc		Creates a daily goal list
+// @route		GET api/logs
+// @desc		Get all logs
+// @access 	Public
+router.get('/', async (req, res) => {
+  try {
+    const logs = await Logs.find();
+    res.json(logs);
+  } catch (error) {
+    console.error(error.msg);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route		POST api/logs
+// @desc		Create a log item
 // @access	Public
-router.post('/', (req, res) => {
-  res.send('Submit a log');
+router.post('/', async (req, res) => {
+  try {
+    const { message, attention, tech } = req.body;
+
+    const newLog = new Logs({
+      message,
+      attention,
+      tech,
+    });
+
+    const log = await newLog.save();
+
+    res.json(log);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
