1d03f280fe8c7c9545784d830aa284f88e8059dc
Tyler Austin <tyler@webguyty.com>
2020-09-28 19:44:16 -0700

    tech git and push working

--- routes/techs.js
+++ routes/techs.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-const Techs = require('../models/techs');
+const Techs = require('../models/Techs');
 
-router.post('/', (req, res) => {
-  res.send('Submit a techs');
+// @route		GET api/logs
+// @desc		Get all logs
+// @access 	Public
+router.get('/', async (req, res) => {
+  try {
+    const techs = await Techs.find();
+    res.json(techs);
+  } catch (error) {
+    console.error(error.msg);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route		POST api/logs
+// @desc		Create a log item
+// @access	Public
+router.post('/', async (req, res) => {
+  try {
+    const { firstName, lastName } = req.body;
+
+    const newTech = new Techs({
+      firstName,
+      lastName,
+    });
+
+    const tech = await newTech.save();
+
+    res.json(tech);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
